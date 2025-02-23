c55e6f9e4b7fc186e76f400b61114840b1ae8681
Juan Cruz Pandolfo Martinez <juancruzpandolfo@gmail.com>
2025-02-23 14:59:28 +0000

    Rutas actualizadas

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,11 +5,48 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
+
+
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
+
+
+@api.route("/signup", methods = ["POST"])
+def signup() :
+    response_body = {}
+    data = request.json
+    user = User(email = data['email'].lower(),
+                password = data['password'],
+                is_active = True)
+    db.session.add(user)
+    db.session.commit()
+    response_body['message'] = "Usuario creado exitosamente!"
+    return response_body,200
+
+
+@api.route("/login", methods = ["POST"])
+def signin() :
+    response_body = {}
+    email = reques.json.get("email",None)
+    password = reques.json.get("password",None)
+    user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+    if user and password == user.password:
+        access_token = create_access_token(identity=[user.username, user.email, user.avatar_url])
+        response_body['access_token'] = access_token
+        response_body['message'] = "Login exitoso!"
+        response_body['results'] = user.serialize()
+        return response_body, 200
+    else:
+        response_body['message'] = "Error, pasword o email incorrectos"
+    return response_body
+
 
 
 @api.route('/hello', methods=['POST', 'GET'])
